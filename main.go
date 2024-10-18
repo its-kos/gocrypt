55d46d56357fbeb0f302e94a9471d1aac352e950
Konstantinos Katserelis <45661771+its-kos@users.noreply.github.com>
2024-10-18 20:21:08 +0200

    Sending bytes to another peer now works

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"context"
+	"crypto/rand"
 	"flag"
 	"fmt"
+	"io"
 	"log"
-	"os"
 
+	"github.com/its-kos/gocrypt/pkg/encryption"
+	"github.com/its-kos/gocrypt/pkg/filechunk"
 	"github.com/its-kos/gocrypt/pkg/network"
 	"github.com/its-kos/gocrypt/pkg/utils"
 
@@ -15,76 +17,45 @@ import (
 	"github.com/libp2p/go-libp2p/core/protocol"
 )
 
-func readData(rw *bufio.ReadWriter) {
-	for {
-		str, err := rw.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading from buffer")
-			panic(err)
-		}
-
-		if str == "" {
-			return
-		}
-		if str != "\n" {
-			// Green console colour: 	\x1b[32m
-			// Reset console colour: 	\x1b[0m
-			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
-		}
-
-	}
-}
-
-func writeData(rw *bufio.ReadWriter) {
-	stdReader := bufio.NewReader(os.Stdin)
-
-	for {
-		fmt.Print("> ")
-		sendData, err := stdReader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading from stdin")
-			panic(err)
-		}
-
-		_, err = rw.WriteString(fmt.Sprintf("%s\n", sendData))
-		if err != nil {
-			fmt.Println("Error writing to buffer")
-			panic(err)
-		}
-		err = rw.Flush()
-		if err != nil {
-			fmt.Println("Error flushing buffer")
-			panic(err)
-		}
-	}
-}
-
 func handleStream(stream libnet.Stream) {
-	fmt.Println("Got a new stream!")
+	buf := make([]byte, 1024)
+	for {
+		n, err := stream.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading from stream:", err)
+			}
+			break
+		}
+		fmt.Printf("Received %d bytes: %v\n", n, buf[:n])
+	}
 
-	// Create a buffer stream for non-blocking read and write.
-	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+	decrypted := make([][]byte, 0)
+	for _, encChunk := range buf {
+		decryptedChunk, err := encryption.DecryptChunk(encChunk, key)
+		if err != nil {
+			log.Fatalf("Error decrypting chunk: %v", err)
+		}
+		decrypted = append(decrypted, decryptedChunk)
+	}
 
-	go readData(rw)
-	go writeData(rw)
+	err := filechunk.StitchFile(decrypted, "./files/testfile_reconstructed.txt")
+	if err != nil {
+		log.Fatalf("Error splitting file: %v", err)
+	}
 
-	// 'stream' will stay open until you close it (or the other side closes it).
+	stream.Close()
 }
 
 type config struct {
-	RendezvousString string
-	ProtocolID       string
-	listenHost       string
-	listenPort       int
+	listenHost string
+	listenPort int
 }
 
 func main() {
 
-	// filePath := "./files/testfile.txt"
-	// chunkSize := 1024 // 1 KB chunks
-	// var wg sync.WaitGroup
-	// ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel()
+	filePath := "./files/testfile.txt"
+	chunkSize := 1024 // 1 KB chunks
 
 	c := &config{}
 
@@ -106,14 +77,32 @@ func main() {
 	}
 	fmt.Printf("Successfully initialized host: %v", host.ID().ShortString())
 
-	// Set a function as stream handler.
-	// This function is called when a peer initiates a connection and starts a stream with this peer.
-	host.SetStreamHandler(protocol.ID("/chat/1.1.0"), handleStream)
+	host.SetStreamHandler(protocol.ID("/gocrypt/chunk-transfer/1.0.0"), handleStream)
+
+	key := make([]byte, 32)
+	_, err = rand.Reader.Read(key)
+	if err != nil {
+		log.Fatalf("Error generating random key: %v", err)
+	}
+
+	chunks, err := filechunk.ChunkFile(filePath, chunkSize)
+	if err != nil {
+		log.Fatalf("Error splitting file: %v", err)
+	}
+
+	encrypted := make([][]byte, 0)
+	for _, chunk := range chunks {
+		encryptedChunk, err := encryption.EncryptChunk(chunk, key)
+		if err != nil {
+			log.Fatalf("Error encrypting chunk: %v", err)
+		}
+		encrypted = append(encrypted, encryptedChunk)
+	}
 
 	peerChan := network.InitMDNS(host, "gocrypt")
 
-	for { // allows multiple peers to join
-		peer := <-peerChan // will block until we discover a peer
+	for {
+		peer := <-peerChan // block until we discover a peer
 		if peer.ID > host.ID() {
 			// if other end peer id greater than us, don't connect to it, just wait for it to connect us
 			fmt.Println("Found peer:", peer, " id is greater than us, wait for it to connect to us")
@@ -127,43 +116,16 @@ func main() {
 		}
 
 		// open a stream, this stream will be handled by handleStream other end
-		stream, err := host.NewStream(ctx, peer.ID, protocol.ID("/chat/1.1.0"))
-
+		stream, err := host.NewStream(ctx, peer.ID, protocol.ID("/gocrypt/chunk-transfer/1.0.0"))
 		if err != nil {
 			fmt.Println("Stream open failed", err)
 		} else {
-			rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-
-			go writeData(rw)
-			go readData(rw)
 			fmt.Println("Connected to:", peer)
+
+			stream.Write(encrypted[0])
+			stream.Close()
 		}
 	}
-	// sigs := make(chan os.Signal, 1) // Buffered cause we don't wanna block, only 1 SIG is enough.
-	// signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	<-sigs
-	// 	fmt.Printf("Received shutdown signal. Closing host %v...\n", host.ID().ShortString())
-	// 	host.Close()
-	// 	cancel()
-	// }()
-
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	for {
-	// 		select {
-	// 		case <-ctx.Done():
-	// 			return
-	// 		default:
-	// 			time.Sleep(2 * time.Second)
-	// 			fmt.Printf("Host %v is running...\n", host.ID().ShortString())
-	// 		}
-	// 	}
-	// }()
 
 	// wg.Wait()
 	// The functionality is stupid but i'm doing it for
@@ -175,79 +137,4 @@ func main() {
 	// communicating the locally saved chunks to the entire
 	// network when it gets online so the DHT will be updated upon
 	// every note reconnect. More overhead but a better solution.
-
-	// key := make([]byte, 32)
-	// _, err := rand.Reader.Read(key)
-	// if err != nil {
-	// 	log.Fatalf("Error generating random key: %v", err)
-	// }
-
-	// chunks, err := filechunk.ChunkFile(filePath, chunkSize)
-	// if err != nil {
-	// 	log.Fatalf("Error splitting file: %v", err)
-	// }
-
-	// encrypted := make([][]byte, 0)
-	// for _, chunk := range chunks {
-	// 	encryptedChunk, err := encryption.EncryptChunk(chunk, key)
-	// 	if err != nil {
-	// 		log.Fatalf("Error encrypting chunk: %v", err)
-	// 	}
-	// 	encrypted = append(encrypted, encryptedChunk)
-	// }
-
-	// err = filechunk.StitchFile(encrypted, "./files/testfile_encrypted.txt")
-	// if err != nil {
-	// 	log.Fatalf("Error splitting file: %v", err)
-	// }
-
-	// decrypted := make([][]byte, 0)
-	// for _, encChunk := range encrypted {
-	// 	decryptedChunk, err := encryption.DecryptChunk(encChunk, key)
-	// 	if err != nil {
-	// 		log.Fatalf("Error decrypting chunk: %v", err)
-	// 	}
-	// 	decrypted = append(decrypted, decryptedChunk)
-	// }
-
-	// err = filechunk.StitchFile(decrypted, "./files/testfile_reconstructed.txt")
-	// if err != nil {
-	// 	log.Fatalf("Error splitting file: %v", err)
-	// }
-
-	// host, err := network.StartNode("/ip4/127.0.0.1/tcp/0")
-	// if err != nil {
-	// 	log.Fatalf("Error creating Host node: %v\n", err)
-	// }
-
-	// sigs := make(chan os.Signal, 1) // Buffered cause we don't wanna block, only 1 SIG is enough.
-	// signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	<-sigs
-	// 	fmt.Printf("Received shutdown signal. Closing host %v...\n", host.ID().ShortString())
-	// 	host.Close()
-	// 	cancel()
-	// }()
-
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	for {
-	// 		select {
-	// 		case <-ctx.Done():
-	// 			return
-	// 		default:
-	// 			time.Sleep(2 * time.Second)
-	// 			fmt.Printf("Host %v is running...\n", host.ID().ShortString())
-	// 		}
-	// 	}
-	// }()
-
-	// wg.Wait()
-	// fmt.Println("Exiting program.")
-
-	//network.Connect(ctx, host, "test")
 }
